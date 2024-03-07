90c1396427489054174edfe73b330d1d29618922
Emmanuel Ochoa <eorivera95@gmail.com>
2024-03-07 14:56:32 -0600

    mallard example

--- Program.cs
+++ Program.cs
@@ -1,2 +1,102 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Duck mallard = new MallardDuck();
+        mallard.PerformQuack();
+        mallard.PerformFly();
+
+    }
+}
+
+public abstract class Duck
+{
+    // reference variables for the behavior interface types.
+    // All duck subclasses inherit these
+    protected IFlyBehavior flyBehavior;
+    protected IQuackBehavior quackBehavior;
+
+    public Duck() { }
+
+    public abstract void Display();
+
+    public void PerformFly()
+    {
+        flyBehavior.Fly();
+    }
+
+    public void PerformQuack()
+    {
+        quackBehavior.Quack();
+    }
+
+    public void swim()
+    {
+        System.Console.WriteLine("All ducks float, even decoys!");
+    }
+}
+
+public interface IFlyBehavior
+{
+    public void Fly();
+
+}
+
+public class FlyWithWings : IFlyBehavior
+{
+    public void Fly()
+    {
+        System.Console.WriteLine("I'm flying!");
+    }
+}
+
+public class FlyNoWay : IFlyBehavior
+{
+    public void Fly()
+    {
+        System.Console.WriteLine("I can't fly");
+    }
+}
+
+public interface IQuackBehavior
+{
+    public void Quack();
+}
+
+public class Quack : IQuackBehavior
+{
+    void IQuackBehavior.Quack()
+    {
+        System.Console.WriteLine("Quack");
+    }
+}
+
+public class MuteQuack : IQuackBehavior
+{
+    public void Quack()
+    {
+        System.Console.WriteLine("<< Silence >>");
+    }
+}
+
+public class Squeak : IQuackBehavior
+{
+    public void Quack()
+    {
+        System.Console.WriteLine("Squeak");
+    }
+}
+
+
+public class MallardDuck : Duck
+{
+    public MallardDuck()
+    {
+        quackBehavior = new Quack();
+        flyBehavior = new FlyWithWings();
+    }
+    public override void Display()
+    {
+        System.Console.WriteLine("I'm a real Mallard duck");
+    }
+}
