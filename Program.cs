faf5a20685de7f7229dd862a98a0426dc9e8bac3
Emmanuel Ochoa <eorivera95@gmail.com>
2024-03-08 15:40:58 -0600

    feat: example 2 of a payment process with strategy pattern

--- Program.cs
+++ Program.cs
@@ -14,6 +14,21 @@ internal class Program
         model.SetFlyBehavior(new FlyRocketPowered());
         model.PerformFly();
 
+        // Client code 
+        PaymentProcessor processor = new PaymentProcessor();
+
+        // Paypal payment 
+        processor.SetPaymentMethod(new PayPalPayment());
+        processor.ProcessPayment();
+
+        //Credit card payment
+        processor.SetPaymentMethod(new CreditCardPayment());
+        processor.ProcessPayment();
+
+        //Bitcoin payment
+        processor.SetPaymentMethod(new BitcoinPayment());
+        processor.ProcessPayment();
+
     }
 }
 
@@ -135,5 +150,73 @@ public class FlyRocketPowered : IFlyBehavior
     public void Fly()
     {
         System.Console.WriteLine("I'm flying with a rocket!");
+    }
+}
+
+
+// Problem Scenario: Payment Processing System
+// You are tasked with implementing a payment processing system for an e-commerce platform.
+//  The platform accepts payments through various payment gateways, including PayPal, Credit Card, and Bitcoin.
+//  Each payment method requires different authentication and transaction processes.
+
+
+class PaymentProcessor
+{
+
+    private IPaymentMethod _paymentMethod;
+
+    public void SetPaymentMethod(IPaymentMethod paymentMethod)
+    {
+        this._paymentMethod = paymentMethod;
+    }
+    public void ProcessPayment()
+    {
+        this._paymentMethod.Authenticate();
+        this._paymentMethod.ProcessTransaction();
+    }
+}
+
+interface IPaymentMethod
+{
+    void Authenticate();
+    void ProcessTransaction();
+}
+
+class PayPalPayment : IPaymentMethod
+{
+    public void Authenticate()
+    {
+        System.Console.WriteLine("PayPal Authentication");
+    }
+
+    public void ProcessTransaction()
+    {
+        System.Console.WriteLine("PayPal Transaction");
+    }
+}
+
+class CreditCardPayment : IPaymentMethod
+{
+    public void Authenticate()
+    {
+        System.Console.WriteLine("Credit card Authentication");
+    }
+
+    public void ProcessTransaction()
+    {
+        System.Console.WriteLine("Credit card Transaction");
+    }
+}
+
+class BitcoinPayment : IPaymentMethod
+{
+    public void Authenticate()
+    {
+        System.Console.WriteLine("Bitcoin Authentication");
+    }
+
+    public void ProcessTransaction()
+    {
+        System.Console.WriteLine("Bitcoin Transaction");
     }
 }
